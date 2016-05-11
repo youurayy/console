0e8be7588a5a34fb02e03df48ef4ffeef9bb0f5b
cbucher <christophe.bucher@laposte.net>
2016-05-11 08:08:17 +0200

    chocolatey package version 1.17.0.16129

--- Packaging/chocolatey/tools/chocolateyInstall.ps1
+++ Packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name = 'ConsoleZ'
-$url = 'https://github.com/cbucher/console/releases/download/1.16.1/ConsoleZ.x86.1.16.1.16068.zip'
-$url64 = 'https://github.com/cbucher/console/releases/download/1.16.1/ConsoleZ.x64.1.16.1.16068.zip'
+﻿$name  = 'ConsoleZ'
+$url   = 'https://github.com/cbucher/console/releases/download/1.17.0/ConsoleZ.x86.1.17.0.16129.zip'
+$url64 = 'https://github.com/cbucher/console/releases/download/1.17.0/ConsoleZ.x64.1.17.0.16129.zip'
 $tools = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 Install-ChocolateyZipPackage $name $url $tools $url64
