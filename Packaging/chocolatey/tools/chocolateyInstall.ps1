655caf14242c095f338ce72cd0fe1594c81736be
cbucher <christophe.bucher@laposte.net>
2017-11-04 13:14:51 +0100

    chocolatey 1.18.2.17272

--- Packaging/chocolatey/tools/chocolateyInstall.ps1
+++ Packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName    = 'ConsoleZ'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/cbucher/console/releases/download/1.18.1/ConsoleZ.x86.1.18.1.17087.zip'
-$url64          = 'https://github.com/cbucher/console/releases/download/1.18.1/ConsoleZ.x64.1.18.1.17087.zip'
-$checksum       = 'B1CFEEBC8426F144AB9D1E050AA250309F89C0E3499DA6370AD1895655015DC3'
+$url            = 'https://github.com/cbucher/console/releases/download/1.18.2/ConsoleZ.x86.1.18.2.17272.zip'
+$url64          = 'https://github.com/cbucher/console/releases/download/1.18.2/ConsoleZ.x64.1.18.2.17272.zip'
+$checksum       = '6FE3F3DAB3E0F1110B9633E63FEDD7ED13A1DA028A8B7E2DF233125C91F5CAD4'
 $checksumType   = 'sha256'
-$checksum64     = 'EDE84C65472F0D1405C721F5898780B1D54E7B98CE8A73CC7D20CD1AA16C31F3'
+$checksum64     = '50D612C3DEA96F07562F25D1C4A69D7BB988B550B7627F31C48BD89DCB106545'
 $checksumType64 = 'sha256'
 
 
