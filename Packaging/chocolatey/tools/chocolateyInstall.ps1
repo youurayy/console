a08ad21f98fbb3edd930f61285935b5234046456
cbucher <christophe.bucher@laposte.net>
2019-05-02 19:29:52 +0200

    chocolatey 1.19.0

--- Packaging/chocolatey/tools/chocolateyInstall.ps1
+++ Packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName    = 'ConsoleZ'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/cbucher/console/releases/download/1.18.3/ConsoleZ.x86.1.18.3.18143.zip'
-$url64          = 'https://github.com/cbucher/console/releases/download/1.18.3/ConsoleZ.x64.1.18.3.18143.zip'
-$checksum       = 'B5265A07FC79C1BCE4FC89EDCB3685CCE4C0A8CCE55163B55C15EBD0C6DD5383'
+$url            = 'https://github.com/cbucher/console/releases/download/1.19.0/ConsoleZ.x86.1.19.0.19104.zip'
+$url64          = 'https://github.com/cbucher/console/releases/download/1.19.0/ConsoleZ.x64.1.19.0.19104.zip'
+$checksum       = 'BAF0D01A6FC2EBA66FE959740045CD268FCDE772B1F3B9F59C4CB1D4D54F252E'
 $checksumType   = 'sha256'
-$checksum64     = '178E5C1A8CB06768E193E80785B82E343BB7379D94C33105B81550120367936A'
+$checksum64     = '671566592F1E8B0B71A5E8A5BCE3A5437A2F65C1E251715B0155774EC1BE669F'
 $checksumType64 = 'sha256'
 
 
